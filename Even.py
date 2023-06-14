b5a7dad50c7f92ae7413fd0194806e4f338916b6
SAIFISHIFA <133900955+SAIFISHIFA@users.noreply.github.com>
2023-06-14 22:03:18 +0530

    Printing even numbers from 1 to N

---
+++ Even.py
@@ -0,0 +1,9 @@
+# InitialCode
+Important project codes related to python
+maximum = int(input(" Please Enter the Maximum Value : "))
+
+for number in range(1, maximum+1):
+
+    if(number % 2 == 0):
+
+        print("{0}".format(number))
