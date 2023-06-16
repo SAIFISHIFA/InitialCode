656fd8f864f9f1c3306a04a4d8605352c048ee49
SAIFISHIFA <133900955+SAIFISHIFA@users.noreply.github.com>
2023-06-16 20:20:30 +0530

    EvaluatingPowerOfANumber

---
+++ PowerOfNumber.py
@@ -0,0 +1,15 @@
+# InitialCode
+Important project codes related to python
+base = 3
+
+exponent = 4
+
+result = 1
+
+while exponent != 0:
+
+    result *= base
+
+    exponent-=1
+
+print("Answer = " + str(result))
